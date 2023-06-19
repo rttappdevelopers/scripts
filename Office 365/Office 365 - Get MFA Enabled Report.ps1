257e7607d81a7d894720acda11b479e68a56db3a
Brad Brown Jr <bradbrownjr@gmail.com>
2023-06-19 15:22:20 -0400

    Added Office 365 audit scripts

---
+++ Office 365/Office 365 - Get MFA Enabled Report.ps1
@@ -0,0 +1,65 @@
+﻿# Get list view and CSV file of active users and their MFA status
+# File export goes to "c:\temp\MFAUsers.csv"
+
+# Search for email addresses among mailboxes and aliases
+Set-ExecutionPolicy RemoteSigned
+
+# Is the MS Online PowerShell module installed? If not, install it and then connect
+Write-Output "Connecting to Micrsoft 365 Azure AD"
+if (!(Get-InstalledModule -Name "MSOnline")) {
+Install-Module -Name MSOnline
+Import-Module MSOnline
+}
+else {
+Import-Module MSOnline
+}
+Connect-MsolService
+
+# From https://www.alitajran.com/export-office-365-users-mfa-status-with-powershell/
+Write-Host "Finding Azure Active Directory Accounts..."
+$Users = Get-MsolUser -All | Where-Object { $_.UserType -ne "Guest" }
+$Report = [System.Collections.Generic.List[Object]]::new() # Create output file
+Write-Host "Processing" $Users.Count "accounts..." 
+ForEach ($User in $Users) {
+
+    $MFADefaultMethod = ($User.StrongAuthenticationMethods | Where-Object { $_.IsDefault -eq "True" }).MethodType
+    $MFAPhoneNumber = $User.StrongAuthenticationUserDetails.PhoneNumber
+    $PrimarySMTP = $User.ProxyAddresses | Where-Object { $_ -clike "SMTP*" } | ForEach-Object { $_ -replace "SMTP:", "" }
+    $Aliases = $User.ProxyAddresses | Where-Object { $_ -clike "smtp*" } | ForEach-Object { $_ -replace "smtp:", "" }
+
+    If ($User.StrongAuthenticationRequirements) {
+        $MFAState = $User.StrongAuthenticationRequirements.State
+    }
+    Else {
+        $MFAState = 'Disabled'
+    }
+
+    If ($MFADefaultMethod) {
+        Switch ($MFADefaultMethod) {
+            "OneWaySMS" { $MFADefaultMethod = "Text code authentication phone" }
+            "TwoWayVoiceMobile" { $MFADefaultMethod = "Call authentication phone" }
+            "TwoWayVoiceOffice" { $MFADefaultMethod = "Call office phone" }
+            "PhoneAppOTP" { $MFADefaultMethod = "Authenticator app or hardware token" }
+            "PhoneAppNotification" { $MFADefaultMethod = "Microsoft authenticator app" }
+        }
+    }
+    Else {
+        $MFADefaultMethod = "Not enabled"
+    }
+  
+    $ReportLine = [PSCustomObject] @{
+        UserPrincipalName = $User.UserPrincipalName
+        DisplayName       = $User.DisplayName
+        MFAState          = $MFAState
+        MFADefaultMethod  = $MFADefaultMethod
+        MFAPhoneNumber    = $MFAPhoneNumber
+        PrimarySMTP       = ($PrimarySMTP -join ',')
+        Aliases           = ($Aliases -join ',')
+    }
+                 
+    $Report.Add($ReportLine)
+}
+
+Write-Host "Report is in c:\temp\MFAUsers.csv"
+$Report | Select-Object UserPrincipalName, DisplayName, MFAState, MFADefaultMethod, MFAPhoneNumber, PrimarySMTP, Aliases | Sort-Object UserPrincipalName | Out-GridView
+$Report | Sort-Object UserPrincipalName | Export-CSV -Encoding UTF8 -NoTypeInformation "c:\temp\MFAUsers.csv"
---
+++ Office 365/Office 365 - Get Mailbox Rules and Forwards.ps1
@@ -0,0 +1,30 @@
+﻿# Get user mail rules and forwarding
+Set-ExecutionPolicy RemoteSigned
+# $includehidden = "-includehidden"
+
+# Is the Exchange Online Management PowerShell module installed? If not, install it
+Write-Output "Connecting to Office 365"
+if (!(Get-InstalledModule -Name "ExchangeOnlineManagement")) {
+        Install-Module -Name ExchangeOnlineManagement -RequiredVersion 1.0.1
+        Import-Module ExchangeOnlineManagement
+    }
+    else {
+        Import-Module ExchangeOnlineManagement
+    }
+
+# Connect to Office 365 platform
+#$globaladmin = Read-Host "Enter Global Administrator username (full e-mail address)"
+Connect-ExchangeOnline # -UserPrincipalName $globaladmin
+
+$userid = Read-Host "Enter mailbox address to fetch rules from, or * for all"
+
+if ($userid -eq "*") {
+        $Mailboxes = Get-Mailbox -ResultSize unlimited  | Where-Object {$_.RecipientTypeDetails -eq "UserMailbox"}
+        foreach ($Mailbox in $Mailboxes){
+            Get-Mailbox $userid  | Select-Object UserPrincipalName,ForwardingSmtpAddress,DeliverToMailboxAndForward | Export-CSV "C:\temp\mailforwards.csv" -Append
+            Get-InboxRule -Mailbox $Mailbox.UserPrincipalName $includehidden | Select-Object MailboxOwnerID,Name,Enabled, priority, Description | Export-CSV "C:\temp\mailrules.csv" -Append
+        }
+    } else {
+        Get-Mailbox $userid  | Select-Object UserPrincipalName,ForwardingSmtpAddress,DeliverToMailboxAndForward | Export-CSV "C:\temp\mailforwards.csv" -Append
+        Get-InboxRule -Mailbox $Mailbox.UserPrincipalName $includehidden | Select-Object MailboxOwnerID,Name,Enabled, priority, Description | Export-CSV "C:\temp\mailrules.csv"
+    }
