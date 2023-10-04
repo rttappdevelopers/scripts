4b84f9d8ea2f3527a83679dbb7e8bf87fa17dd5a
Brad Brown Jr <bradbrownjr@gmail.com>
2023-10-04 10:20:19 -0400

    Updated output feedback

--- Windows/Windows - User Profile Audit.ps1
+++ Windows/Windows - User Profile Audit.ps1
@@ -11,47 +11,25 @@ $extexclude = $env:extexclude
 # Get current user
 $CurrentUser = (Get-WmiObject -Class Win32_ComputerSystem).UserName
 $CurrentUser = $CurrentUser -replace ".*\\"
-Write-Output $CurrentUser
+Write-Output "Current user: $CurrentUser"
 
 # Get last logged in user and strip the preceding
 $LastUser = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI" -Name "LastLoggedOnUser").LastLoggedOnUser
 $LastUser = $LastUser -replace ".*\\"
-Write-Output $LastUser
+Write-Output "Last user: $LastUser"
 
 # List exclusions
 $ExcludeValues=@("Public", "Administrator", "Guest", "MSSQL$MICROSOFT##WID", ".NET v4.5", $extexclude, $CurrentUser, $LastUser) # Add any other common admin accounts here
 
-$CutoffDate = (get-date).AddDays(-30).Date
-Write-Output "Username`t`tLast Logon`t`tCutoff Date`t`tAction"
 Get-ChildItem C:\users | ForEach-Object { 
     $user = $_.Name
-    If($user -in $ExcludeValues){return}
-    
-    $lastlogon = net user $_.Name $domain | Select-String "Last logon"
-    if ($lastlogon -match '\d+/\d+/\d{4}\s{1}\d+:\d+:\d{2}' -eq "True") {
-        $dateout = $Matches[0]
-        
-        if ([DateTime]$dateout -lt [DateTime]$CutoffDate) { 
-            if ($assessonly -ne "true") {
-                Write-Output "$user `t $dateout `t $cutoffdate, deleting" 
-                Get-CimInstance -Class Win32_UserProfile | Where-Object {$_.LocalPath -like "*$user*"} | Remove-CimInstance
-            }
-            else {
-                Write-Output "$user `t $dateout `t $cutoffdate, suggest deleting"
-            }
-        }
-        else
-        {
-            Write-Output "$user `t $dateout `t $cutoffdate, keeping"
-        }
+    If($user -in $ExcludeValues){return}   
+
+    if ($assessonly -ne "true") {
+        Write-Output "Deleting user $user" 
+        Get-CimInstance -Class Win32_UserProfile | Where-Object {$_.LocalPath -like "*$user*"} | Remove-CimInstance
     }
     else {
-        if ($assessonly -ne "true") {
-            Write-Output "$user not on file, deleting."
-            Get-CimInstance -Class Win32_UserProfile | Where-Object {$_.LocalPath -like "*$user*"} | Remove-CimInstance
-        }
-        else {
-            Write-Output "$user not on file, suggest deleting."           
-        }
+        Write-Output "Suggest deleting $user"
     }
 }
