c04831990a59d5bab48bf1c5a5f7e2178bbfaad7
Brad Brown Jr <bradbrownjr@gmail.com>
2024-01-11 09:46:05 -0500

    Added confirmation output

--- Windows/Windows - Promote current user to local admin.ps1
+++ Windows/Windows - Promote current user to local admin.ps1
@@ -4,3 +4,6 @@ Write-Output "Current user: $CurrentUser"
 
 # Add current user to local administrators group
 net localgroup administrators $CurrentUser /add
+
+# Check if the user is a member of the Users group
+net localgroup administrators
